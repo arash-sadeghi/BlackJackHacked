11164372077abbf065f350232ff58b05fcd4e1d0
arash sadeghi <arash.sadeghi1997.com>
2023-03-26 00:35:35 -0230

    recorder added to player

--- Game.py
+++ Game.py
@@ -9,17 +9,18 @@ def print2(inp,color='white',attrs=[]):
     logging.info(inp)
 
 if __name__ == '__main__':
+    logName = ctime(time()).replace(" ","_").replace(":","_")
+    dir = 'logs'
+    logName = os.path.join(dir,logName)
+    logging.basicConfig(filename=logName+'.log', level=logging.DEBUG)
+
     points = {'As': 1, 'A': 11, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10} #! Ace is just 11 for now
-    player = Player(points)
+    player = Player(points,logName)
     dealer = Dealer(points)
     wins = 0
     losses = 0
     pushes = 0
     numberOfGames = 100
-    logName = ctime(time()).replace(" ","_").replace(":","_")+'.log'
-    dir = 'logs'
-    logName = os.path.join(dir,logName)
-    logging.basicConfig(filename=logName, level=logging.DEBUG)
 
     for gameIt in range(1,numberOfGames+1): #! just to avoid game number 0
 
@@ -31,13 +32,15 @@ if __name__ == '__main__':
             
         print2("\n"+"-"*20+f" game {gameIt} "+"-"*20+"\n")
         cardsOnTable = dealer.deal()
+        print2(f'[game] dealer hand {cardsOnTable[0]} ','cyan',attrs=['bold'])
+        print2(f'[game] player hand {[_ for _ in cardsOnTable[1:]]} , sum {sum([points[_] for _ in cardsOnTable[1:]])}','light_green',attrs=['bold'])
+
         if dealer.checkPlayerBJ() == 1:
             print2("[game] Player BlackJack",'green')
+            wins += 1
             dealer.endHand() #! no result is given for player since it was pure luck of player
             continue
 
-        print2(f'[game] dealer hand {cardsOnTable[0]} ','cyan',attrs=['bold'])
-        print2(f'[game] player hand {[_ for _ in cardsOnTable[1:]]} , sum {sum([points[_] for _ in cardsOnTable[1:]])}','light_green',attrs=['bold'])
         decision = player.decide(cardsOnTable)
         print2(f'[game] decision {decision}')
         
@@ -53,7 +56,7 @@ if __name__ == '__main__':
             decision = player.decide(cardsOnTable)
             result = dealer.takeAction(decision)
 
-        player.learn(cardsOnTable , decision , result)
+        player.record(cardsOnTable , decision , result)
 
         print2(f"[game] playerHand {dealer.playerHand}  {sum([ points[_] for _ in dealer.playerHand])} dealerHand {dealer.dealerHand} {sum([ points[_] for _ in dealer.dealerHand])}")
 
--- Player.py
+++ Player.py
@@ -1,10 +1,13 @@
-
+import numpy as np
 
 class Player:
-    def __init__(self,points):
+    def __init__(self,points,logName):
         self.points = points
+        self.softTable = np.zeros((21-2+1, 11-2+1 , 2 , 3)) #! sums * dealer card * hit/stay * win/loss/push
+        self.hardTable = np.zeros((21-2+1, 11-2+1 , 2 , 3)) 
+        self.fileName = logName
+
     def decide(self,cards):
-        playerSum = sum([self.points[_] for _ in cards[1:]])
         key = input('[player] decide >>> ')
         if key == 'h':
             return 'hit'
@@ -12,5 +15,38 @@ class Player:
             return 'stay'
 
         pass
-    def learn(self,*args):
-        pass
+    def record(self,cardsOnTable , decision , result):
+        dealerCard = cardsOnTable[0]
+        dealerCardValue = self.points[dealerCard]
+        playerCards = cardsOnTable[1:]
+        playerCardsSum = sum([self.points[_] for _ in playerCards])
+        if 'A' in playerCards:
+            actionIndex = 0 if decision == 'hit' else 1
+            if result == 'playerWon':
+                resultIndex = 0 
+            elif result == 'playerLost':
+                resultIndex = 1 
+            elif result == 'push':
+                resultIndex = 2
+            else:
+                raise NameError("[player] invalid result")
+
+            self.softTable[playerCardsSum-2][dealerCardValue-2][actionIndex][resultIndex] += 1 
+
+        else: 
+            actionIndex = 0 if decision == 'hit' else 1
+            if result == 'playerWon':
+                resultIndex = 0 
+            elif result == 'playerLost':
+                resultIndex = 1 
+            elif result == 'push':
+                resultIndex = 2
+            else:
+                raise NameError("[player] invalid result")
+
+            self.hardTable[playerCardsSum-2][dealerCardValue-2][actionIndex][resultIndex] += 1 
+    
+    def saveRecords(self):
+        np.save("logs/playerhardTable"+self.fileName+".npy",self.hardTable)
+        np.save("logs/playersoftTable"+self.fileName+".npy",self.softTable)
+
